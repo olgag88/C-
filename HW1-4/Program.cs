0c190d28d9e37ab5a64b0ae017275e0a785065ed
Olga <olgag88@bk.ru>
2022-11-16 11:35:36 +0300

    HW1-4

---
+++ HW1-4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int[] mass = new int[3];
+int n = mass.Length;
+int i = 0;
+int max = mass[0];
+Console.WriteLine("Введите числа:");
+while (i<n)
+{
+    mass[i] = Convert.ToInt32(Console.ReadLine());
+    i++;
+}
+for (i=1; i<n;i++)
+{
+    if(mass[i]>max) max = mass[i];
+}
+Console.WriteLine("Максимальное число: {0}", max);
