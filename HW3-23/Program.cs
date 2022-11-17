52cc28abdfa216447f3fb4532cb75b75511c7d88
Olga <olgag88@bk.ru>
2022-11-17 10:28:10 +0300

    HW3-23

---
+++ HW3-23/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите число для нахождения ряда кубов чисел до него начиная с 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] mass = new int[a];
+int i;
+for (i=0; i<a; i++)
+{
+    mass[i] = (i+1)*(i+1)*(i+1);
+    Console.WriteLine("{0} {1}", i+1, mass[i]);
+}
