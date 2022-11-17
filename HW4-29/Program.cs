79578f23fc1b81a1d728fa14f91c83592e39d369
Olga <olgag88@bk.ru>
2022-11-17 11:08:27 +0300

    HW4-29

---
+++ HW4-29/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+
+Console.Write("Введите длинну массмва N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] mass = new int[N];
+int i;
+Console.WriteLine("Введите числа:");
+for (i=0; i<mass.Length;i++) mass[i] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введенные числа:");
+for (i=0; i<mass.Length;i++) Console.Write("{0} ", mass[i]);
+
+
