01935a993d0ec3231c5e2e4234b6dad8cb355e09
Olga <olgag88@bk.ru>
2022-11-21 18:17:37 +0300

    HW7-52

---
+++ HW7-52/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+void RandomMatrix(int[,] mass)
+{
+    for (int i = 0; i < mass.GetLength(0); i++)
+    {
+        for (int j = 0; j < mass.GetLength(1); j++)
+        {
+            mass[i, j] = new Random().Next(1,10);
+        }
+    }
+}
+
+void PrintMatrix(int[,] mass)
+{
+    for (int i = 0; i < mass.GetLength(0); i++)
+    {
+        for (int j = 0; j < mass.GetLength(1); j++)
+        {
+            Console.Write("{0,6:F1}", mass[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+
+double[] SrArifmStolb(int[,] mass)
+{
+    int m = 0;
+    double[] massiv = new double[mass.GetLength(1)];
+    double sum;
+    while (m < mass.GetLength(1)) // Психолгически удобнее использовать while
+    {
+        sum =0;
+        for (int i = 0; i < mass.GetLength(0); i++)
+        {
+            sum += mass[i, m];
+        }
+        massiv[m] =sum/mass.GetLength(0); 
+        m++;
+    }
+    return massiv;
+}
+
+void Vyvod(double[] mass)
+{
+    Console.WriteLine("Среднее арифметическое столбцов:");
+    for (int j = 0; j < mass.Length; j++)
+    {
+        Console.Write("{0,6:F2} ", mass[j]);
+    }
+}
+
+Console.WriteLine("Введите количество строк матрицы (n): ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов матрицы (n): ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, m];
+
+RandomMatrix(matrix);
+PrintMatrix(matrix);
+Vyvod(SrArifmStolb(matrix));
