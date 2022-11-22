352287998cf17c78b030497c4c60aae5ecfdda0d
Olga <olgag88@bk.ru>
2022-11-22 10:45:47 +0300

    HW9-66

---
+++ HW9-66/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значения M и N. Напишите программу, которая определяет количество натуральных элементов в промежутке от M до N.
+
+Console.WriteLine("Введите натуральное число от которого считать:");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число до которого считать:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int SumNaturElem (int m, int n, int sum)
+{
+    if (m > n) 
+    { 
+        return sum;
+    }
+    sum = sum + (m++);
+    return SumNaturElem(m, n, sum);
+}
+
+Console.WriteLine($"Сумма от натуральных чисел от {M} до {N} равна: {SumNaturElem(M, N, 0)}");
